04ea27c97411e6f8212aa55d5d2b525f8201fd20
Avishka Dhanajaya <avishka0303@gmail.com>
2019-03-07 22:00:23 +0530

    create a new user & email verification is done

--- app/src/main/java/com/example/predatorx21/fireemailauth/MainActivity.java
+++ app/src/main/java/com/example/predatorx21/fireemailauth/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.predatorx21.fireemailauth;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -12,6 +20,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final String TAG="MainActivity";
     private FirebaseAuth mAuth;
 
+    private EditText emailTxt,passTxt;
     //initialize android lifecycle states .-----------------------------------------------
 
     @Override
@@ -21,8 +30,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //get an instance of firebase auth.
         mAuth = FirebaseAuth.getInstance();
+        findViewById(R.id.everifyBtn).setOnClickListener(this);
+        findViewById(R.id.signNewbtn).setOnClickListener(this);
 
-
+        emailTxt=findViewById(R.id.emailF);
+        passTxt=findViewById(R.id.passF);
 
     }
 
@@ -37,8 +49,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        if(view.getId()==R.id.everifyBtn){
 
+        String email=emailTxt.getText().toString();
+        String pass=passTxt.getText().toString();
+
+        if(view.getId()==R.id.everifyBtn){
+            emailVerification();
+        }else if(view.getId()==R.id.signNewbtn){
+            createAccount(email,pass);
         }
+    }
+
+    private void createAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    //updateUI(user);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                    //updateUI(null);
+                }
+            }
+        });
+    }
+
+    private void emailVerification() {
+
+        findViewById(R.id.everifyBtn).setEnabled(false);
+        final FirebaseUser user=mAuth.getCurrentUser();
+        user.sendEmailVerification()
+            .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    findViewById(R.id.everifyBtn).setEnabled(true);
+
+                    if (task.isSuccessful()) {
+                        Toast.makeText(MainActivity.this,
+                                "Verification email sent to " + user.getEmail(),
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        Log.e(TAG, "sendEmailVerification", task.getException());
+                        Toast.makeText(MainActivity.this,
+                                "Failed to send verification email.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
     }
 }
