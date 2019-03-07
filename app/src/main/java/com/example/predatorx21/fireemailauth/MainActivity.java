a9bced029991882947f98d6318ad5802c3b5305c
Avishka Dhanajaya <avishka0303@gmail.com>
2019-03-07 21:30:53 +0530

    initializing firebase auth

--- app/src/main/java/com/example/predatorx21/fireemailauth/MainActivity.java
+++ app/src/main/java/com/example/predatorx21/fireemailauth/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.predatorx21.fireemailauth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG="MainActivity";
+    private FirebaseAuth mAuth;
+
+    //initialize android lifecycle states .-----------------------------------------------
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get an instance of firebase auth.
+        mAuth = FirebaseAuth.getInstance();
+
+
+
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.everifyBtn){
+
+        }
+    }
 }
